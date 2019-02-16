d35ef26146364b4595043bdda13e76fc84231ced
Hao Bai <baihaozi12@gmail.com>
2019-02-16 17:14:42 +0000

    c

--- assignment1files/Agent.java
+++ assignment1files/Agent.java
@@ -1,4 +1,7 @@
+
 import java.awt.Color;
+import java.awt.Point;
+
 
 /** The random gomoku player chooses random squares on the board (using a
  *  uniform distribution) until an unoccupied square is found, which is then
@@ -8,12 +11,40 @@ import java.awt.Color;
  **/
 class Agent extends GomokuPlayer {
 
+    private int score_get;
+
+    public Point bestMove;
+
+    private int depth;
+
+    // private final int[][] history;
+
+
     public Move chooseMove(Color[][] board, Color me) {
+
+
         while (true) {
             int row = (int) (Math.random() * 8);	// values are from 0 to 7
             int col = (int) (Math.random() * 8);
-            if (board[row][col] == null)			// is the square vacant?
-                return new Move(row, col);
+//            if (board[row][col] == null)			// is the square vacant?
+//                return new Move(row, col);
+            // check first move
+            int our_color_num = 0;
+            for(int i = 0;i<8;i++ ) {
+                for (int j = 0; j < 8; j++) {
+                    if(board[i][j] == me){
+                        our_color_num++;
+                    }
+                }
+            }
+            // first move
+            if(our_color_num == 0){
+                int i = 3 + (int)(Math.random() * (4-3+1));// values are from 3 to 4
+                int j = 3 + (int)(Math.random() * (4-3+1));// values are from 3 to 4
+                return new Move(i, j);
+            }
+
+
 
 
             // from here
@@ -36,14 +67,192 @@ class Agent extends GomokuPlayer {
                 }
             }
 
+
+
+
             if (board[row][col] == null)			// is the square vacant?
                 return new Move(row, col);
         }
     } // chooseMove()
 
+    public static int is_Game_Over(Color[][] board, Color me){
+        // 0 draw
+        // 1 we win
+        // 2 we loss
+        int is_Game_Over = 0;
+        Color color;
+        for(int i = 0;i<8;i++ ){
+            for(int j = 0;j<8;j++ ){
+                if(board[i][j] != null){
+                    color = board[i][j];
+                    if(color == me){
+                        int value = 1;
+                        if(j + 1 < 8){//right
+                            if(board[i ][j+1] == me){
+                                value += search_right(board, i,j+1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(i + 1 < 8){//down
+                            if(board[i +1][j] == me){
+                                value += search_down(board, i+1,j,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(j - 1 >= 0){//left
+                            if(board[i ][j-1] == me){
+                                value += search_left(board, i,j-1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(i - 1 >= 0){//up
+                            if(board[i-1 ][j] == me){
+                                value += search_up(board, i -1,j,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+
+                        }
+                        else if(j + 1 < 8 && i + 1 <8){//down right
+                            if(board[i+1][j+1] == me){
+                                value += search_down_right(board, i + 1,j + 1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(j - 1 >= 0 && i + 1 <8){//down left
+                            if(board[i+1][j-1] == me){
+                                value += search_down_left(board, i + 1,j - 1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(j - 1 >= 0 && i - 1 >= 0){//up left
+                            if(board[i -1][j-1] == me){
+                                value += search_up_left(board, i - 1,j - 1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+                        else if(j + 1 < 8 && i - 1 >= 0){//up right
+                            if(board[i -1][j + 1] == me){
+                                value += search_up_right(board, i - 1,j + 1,me);
+                            }
+                            if(value == 5){
+                                return 1;
+                            }
+                        }
+
+                        // else others
+                    }else {
+
+                        int value = 1;
+                        if(j + 1 < 8){//right
+                            if(board[i ][j+1] != me && board[i ][j+1] != null){
+                                value += search_right_forOther(board, i,j+1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                        else if(i + 1 < 8){//down
+                            if(board[i+1 ][j] != me && board[i+1 ][j] != null){
+                                value += search_down_forOther(board, i+1,j,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                        else if(j - 1 >= 0){//left
+                            if(board[i ][j-1] != me && board[i ][j-1] != null ){
+                                value += search_left_forOthers(board, i,j-1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                        else if(i - 1 >= 0){//up
+                            if(board[i-1 ][j] != me && board[i-1 ][j] != null){
+                                value += search_up_forOthers(board, i -1,j,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                        else if(j + 1 < 8 && i + 1 <8){//down right
+                            if(board[i+1][i+1] != me && board[i+1][j+1] != null){
+                                value += search_down_right_forOther(board, i + 1,j + 1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                        else if(j - 1 >= 0 && i + 1 <8){//down left
+                            if(board[i+1][j-1] != me && board[i+1][j+1] != null){
+                                value += search_down_left_forOthers(board, i + 1,j - 1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+
+                        else if(j - 1 >= 0 && i - 1 >= 0){//up left
+                            if(board[i -1][j-1] != me && board[i -1][j-1] != null ){
+                                value += search_up_left_forOthers(board, i - 1,j - 1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+
+                        else if(j + 1 < 8 && i - 1 >= 0){//up right
+                            if(board[i -1][j + 1] != me && board[i -1][j + 1] != null){
+                                value += search_up_right_fotOthers(board, i - 1,j + 1,me);
+                            }
+                            if(value == 5){
+                                return 2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return is_Game_Over;
+    }
+
+    public static Point alpha_beta_main(Color[][] board, int alpha, int beta, Color me, Point bestMove){
 
 
 
+        return bestMove;
+    }
+
+    public static Point alpha_beta_search(Color[][] board, int row, int col, Color me, Point bestMove){
+
+
+
+        return bestMove;
+    }
+
+    /**
+     *
+     * @param board
+     * @param row
+     * @param col
+     * @param me
+     * @return
+     */
     public static int get_value(Color[][] board, int row, int col, Color me){
         int value = 0;
         if(col + 1 < 8){//right
