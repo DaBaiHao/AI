b6971ebdd50a380bbf9cf5a1163c4001c61b446e
Hao Bai <baihaozi12@gmail.com>
2019-02-16 00:11:13 +0000

    c

--- assignment1files/Agent.java
+++ assignment1files/Agent.java
@@ -17,75 +17,88 @@ class Agent extends GomokuPlayer {
 
 
             // from here
-            int list[][] = new int[8][8];
+            int list_my_value[][] = new int[8][8];
+            int list_other_value[][] = new int[8][8];
             // initialization
             for(int i = 0;i<8;i++ ){
                 for(int j = 0;j<8;j++ ){
-                    list[i][j] = 0;
+                    list_my_value[i][j] = 0;
+                    list_other_value[i][j] = 0;
                 }
             }
             // search all board
             for(int i = 0;i<8;i++ ){
                 for(int j = 0;j<8;j++ ){
-
+                    if (board[row][col] == null){
+                        list_my_value[i][j] = get_value(board,i,j,me);
+                        list_other_value[i][j] = get_value_forOther(board,i,j,me);
+                    }
                 }
             }
+
+            if (board[row][col] == null)			// is the square vacant?
+                return new Move(row, col);
         }
     } // chooseMove()
 
+
+
+    
     public static int get_value(Color[][] board, int row, int col, Color me){
         int value = 0;
-        if(row + 1 < 8){//right
-            if(board[row + 1][col] == me){
-                value += search_right(board,row,col,me);
-            }
-        }
-        if(col + 1 < 8){//down
+        if(col + 1 < 8){//right
             if(board[row ][col+1] == me){
-                value += search_down(board,row,col,me);
+                value += search_right(board, row,col+1,me);
             }
         }
-        if(row - 1 >= 0){//left
-            if(board[row + 1][col] == me){
-                value += search_left(board,row,col,me);
+        if(row + 1 < 8){//down
+            if(board[row +1][col] == me){
+                value += search_down(board, row+1,col,me);
             }
         }
-        if(col - 1 >= 0){//up
-            if(board[row ][col+1] == me){
+        if(col - 1 >= 0){//left
+            if(board[row ][col-1] == me){
+                value += search_left(board, row,col-1,me);
+            }
+        }
+        if(row - 1 >= 0){//up
+            if(board[row-1 ][col] == me){
                 value += search_up(board, row -1,col,me);
             }
         }
         if(col + 1 < 8 && row + 1 <8){//down right
             if(board[row+1][col+1] == me){
-                value += search_down_right()right(board,row,col,me);
+                value += search_down_right(board, row + 1,col + 1,me);
             }
         }
         if(col - 1 >= 0 && row + 1 <8){//down left
             if(board[row+1][col-1] == me){
-
+                value += search_down_left(board, row + 1,col - 1,me);
             }
         }
 
         if(col - 1 >= 0 && row - 1 >= 0){//up left
             if(board[row -1][col-1] == me){
-
+                value += search_up_left(board, row - 1,col - 1,me);
             }
         }
 
         if(col + 1 < 8 && row - 1 >= 0){//up right
             if(board[row -1][col + 1] == me){
-
+                value += search_up_right(board, row - 1,col + 1,me);
             }
         }
 
 
-        return 0;
+        return value;
     }
 
     public static int search_up(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(row -1>=0){
-            value += search_up(board, row -1,col,me);
+            if(board[row-1 ][col] == me) {
+                value += search_up(board, row - 1, col, me);
+            }
         }
         return value;
     }
@@ -93,7 +106,9 @@ class Agent extends GomokuPlayer {
     public static int search_down(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(row + 1 < 8){
-            value += search_down(board, row+1,col,me);
+            if(board[row +1][col] == me) {
+                value += search_down(board, row + 1, col, me);
+            }
         }
         return value;
     }
@@ -101,7 +116,9 @@ class Agent extends GomokuPlayer {
     public static int search_right(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col + 1 < 8){
-            value += search_right(board, row,col+1,me);
+            if(board[row ][col+1] == me) {
+                value += search_right(board, row, col + 1, me);
+            }
         }
         return value;
     }
@@ -109,7 +126,9 @@ class Agent extends GomokuPlayer {
     public static int search_left(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col - 1 >= 0){
-            value += search_left(board, row,col-1,me);
+            if(board[row ][col-1] == me) {
+                value += search_left(board, row, col - 1, me);
+            }
         }
         return value;
     }
@@ -117,7 +136,9 @@ class Agent extends GomokuPlayer {
     public static int search_down_right(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col + 1 < 8 && row + 1 <8){
-            value += search_down_right(board, row + 1,col + 1,me);
+            if(board[row+1][col+1] == me) {
+                value += search_down_right(board, row + 1, col + 1, me);
+            }
         }
         return value;
     }
@@ -125,7 +146,9 @@ class Agent extends GomokuPlayer {
     public static int search_down_left(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col - 1 >= 0 && row + 1 <8){
-            value += search_down_left(board, row + 1,col - 1,me);
+            if(board[row+1][col-1] == me) {
+                value += search_down_left(board, row + 1, col - 1, me);
+            }
         }
         return value;
     }
@@ -133,7 +156,9 @@ class Agent extends GomokuPlayer {
     public static int search_up_left(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col - 1 >= 0 && row - 1 >= 0){
-            value += search_up_left(board, row - 1,col - 1,me);
+            if(board[row -1][col-1] == me) {
+                value += search_up_left(board, row - 1, col - 1, me);
+            }
         }
         return value;
     }
@@ -141,9 +166,144 @@ class Agent extends GomokuPlayer {
     public static int search_up_right(Color[][] board,int row, int col, Color me){
         int value = 1;
         if(col + 1 < 8 && row - 1 >= 0){
-            value += search_up_right(board, row - 1,col + 1,me);
+            if(board[row -1][col + 1] == me) {
+                value += search_up_right(board, row - 1, col + 1, me);
+            }
         }
         return value;
     }
 
+
+    //
+    //
+    //
+
+    public static int get_value_forOther(Color[][] board, int row, int col, Color me){
+        int value = 0;
+        if(col + 1 < 8){//right
+            if(board[row ][col+1] != me && board[row ][col+1] != null){
+                value += search_right_forOther(board, row,col+1,me);
+            }
+        }
+        if(row + 1 < 8){//down
+            if(board[row+1 ][col] != me && board[row+1 ][col] != null){
+                value += search_down_forOther(board, row+1,col,me);
+            }
+        }
+        if(col - 1 >= 0){//left
+            if(board[row ][col-1] != me && board[row ][col-1] != null ){
+                value += search_left_forOthers(board, row,col-1,me);
+            }
+        }
+        if(row - 1 >= 0){//up
+            if(board[row-1 ][col] != me && board[row-1 ][col] != null){
+                value += search_up_forOthers(board, row -1,col,me);
+            }
+        }
+        if(col + 1 < 8 && row + 1 <8){//down right
+            if(board[row+1][col+1] != me && board[row+1][col+1] != null){
+                value += search_down_right_forOther(board, row + 1,col + 1,me);
+            }
+        }
+        if(col - 1 >= 0 && row + 1 <8){//down left
+            if(board[row+1][col-1] != me && board[row+1][col+1] != null){
+                value += search_down_left_forOthers(board, row + 1,col - 1,me);
+            }
+        }
+
+        if(col - 1 >= 0 && row - 1 >= 0){//up left
+            if(board[row -1][col-1] != me && board[row -1][col-1] != null ){
+                value += search_up_left_forOthers(board, row - 1,col - 1,me);
+            }
+        }
+
+        if(col + 1 < 8 && row - 1 >= 0){//up right
+            if(board[row -1][col + 1] != me && board[row -1][col + 1] != null){
+                value += search_up_right_fotOthers(board, row - 1,col + 1,me);
+            }
+        }
+
+
+        return value;
+    }
+
+    public static int search_up_forOthers(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(row -1>=0){
+            if(board[row-1 ][col] != me && board[row-1 ][col] != null) {
+                value += search_up_forOthers(board, row - 1, col, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_down_forOther(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(row + 1 < 8){
+            if(board[row+1 ][col] != me && board[row+1 ][col] != null){
+                value += search_down_forOther(board, row + 1, col, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_right_forOther(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col + 1 < 8){
+            if(board[row ][col+1] != me && board[row ][col+1] != null){
+                value += search_right_forOther(board, row, col + 1, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_left_forOthers(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col - 1 >= 0){
+            if(board[row ][col-1] != me && board[row ][col-1] != null) {
+                value += search_left_forOthers(board, row, col - 1, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_down_right_forOther(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col + 1 < 8 && row + 1 <8){
+            if(board[row+1][col+1] != me && board[row+1][col+1] != null) {
+                value += search_down_right_forOther(board, row + 1, col + 1, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_down_left_forOthers(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col - 1 >= 0 && row + 1 <8){
+            if(board[row+1][col-1] != me && board[row+1][col-1] != null) {
+                value += search_down_left_forOthers(board, row + 1, col - 1, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_up_left_forOthers(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col - 1 >= 0 && row - 1 >= 0){
+            if(board[row -1][col-1] != me && board[row -1][col-1] != null) {
+                value += search_up_left_forOthers(board, row - 1, col - 1, me);
+            }
+        }
+        return value;
+    }
+
+    public static int search_up_right_fotOthers(Color[][] board,int row, int col, Color me){
+        int value = 1;
+        if(col + 1 < 8 && row - 1 >= 0){
+            if(board[row -1][col + 1] != me && board[row -1][col + 1] != null) {
+                value += search_up_right_fotOthers(board, row - 1, col + 1, me);
+            }
+        }
+        return value;
+    }
 } //
